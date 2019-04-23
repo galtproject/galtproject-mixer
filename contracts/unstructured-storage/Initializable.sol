ee8fa8031d42bdd56bceac9f57898deb22abfbf1
Nikita Chebykin <nikita@chebyk.in>
2019-04-23 19:13:09 +0200

    Merge master; Update files to solidity v0.5.7

---
+++ contracts/unstructured-storage/Initializable.sol
@@ -0,0 +1,41 @@
+/*
+ * Copyright ©️ 2018 Galt•Space Society Construction and Terraforming Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka),
+ * [Dima Starodubcev](https://github.com/xhipster),
+ * [Valery Litvin](https://github.com/litvintech) by
+ * [Basic Agreement](http://cyb.ai/QmSAWEG5u5aSsUyMNYuX2A2Eaz4kEuoYWUkVBRdmu9qmct:ipfs)).
+ *
+ * Copyright ©️ 2018 Galt•Core Blockchain Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) and
+ * Galt•Space Society Construction and Terraforming Company by
+ * [Basic Agreement](http://cyb.ai/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS:ipfs)).
+ */
+pragma solidity 0.5.7;
+
+
+/**
+ * @title Initializable
+ * @dev Simple helper contract to support initialization outside of the constructor.
+ * To use it, replace the constructor with a function that has the
+ * `isInitializer` modifier.
+ * WARNING: This helper does not support multiple inheritance.
+ * WARNING: It is the developer's responsibility to ensure that an initializer
+ * is actually called.
+ * Use `Migratable` for more complex migration mechanisms.
+ */
+contract Initializable {
+
+  /**
+   * @dev Indicates if the contract has been initialized.
+   */
+  bool public initialized;
+
+  /**
+   * @dev Modifier to use in the initialization function of a contract.
+   */
+  modifier isInitializer() {
+    require(!initialized, "Contract instance has already been initialized");
+    _;
+    initialized = true;
+  }
+}
---
+++ contracts/unstructured-storage/Proxy.sol
@@ -0,0 +1,52 @@
+/*
+ * Copyright ©️ 2018 Galt•Space Society Construction and Terraforming Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka),
+ * [Dima Starodubcev](https://github.com/xhipster),
+ * [Valery Litvin](https://github.com/litvintech) by
+ * [Basic Agreement](http://cyb.ai/QmSAWEG5u5aSsUyMNYuX2A2Eaz4kEuoYWUkVBRdmu9qmct:ipfs)).
+ *
+ * Copyright ©️ 2018 Galt•Core Blockchain Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) and
+ * Galt•Space Society Construction and Terraforming Company by
+ * [Basic Agreement](http://cyb.ai/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS:ipfs)).
+ */
+
+pragma solidity 0.5.7;
+
+
+/**
+ * @title AbstractProxy
+ * @dev Gives the possibility to delegate any call to a foreign implementation.
+ */
+contract Proxy {
+  /**
+  * @dev Tells the address of the implementation where every call will be delegated.
+  * Should be implemented in a descendant contract
+  * @return address of the implementation to which it will be delegated
+  */
+  function implementation() public view returns (address) {
+    assert(false);
+  }
+
+  /**
+  * @dev Fallback function allowing to perform a delegatecall to the given implementation.
+  * This function will return whatever the implementation call returns
+  */
+  function () payable external {
+    address _impl = implementation();
+    require(_impl != address(0));
+
+    assembly {
+      let ptr := mload(0x40)
+      calldatacopy(ptr, 0, calldatasize)
+      let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
+      let size := returndatasize
+      returndatacopy(ptr, 0, size)
+
+      switch result
+      case 0 { revert(ptr, size) }
+      default { return(ptr, size) }
+    }
+  }
+}
+
